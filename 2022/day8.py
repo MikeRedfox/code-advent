de0b8a2a36e29926b34207d031b6250654316096
MikeRedfox <mikiguarda@gmail.com>
2025-11-25 12:29:35 +0100

    add day8 part 2 2022

--- 2022/day8.py
+++ 2022/day8.py
@@ -13,7 +13,7 @@ def part1():
     n = len(data)
     m = len(data[1])
     visible = 2*((n - 1) + (m - 1))
-    print(data,n,m)
+    # print(data,n,m)
     for i in range(1, n-1):
         for j in range(1, m-1):
             # print(data[i][j])
@@ -30,8 +30,58 @@ def part1():
 # >>> max(a[2][1:3])
 # 5
 
-print(part1())
+def get_view_score(m, i, j):
+    rl = 0
+    rr = 0
+    cu = 0
+    cd = 0
+    for elem in m[i][j-1::-1]:
+        if m[i][j] <= elem:
+            rl += 1
+            break
+        elif m[i][j] > elem:
+            rl += 1
+    for elem in m[i][j+1:]:
+        if m[i][j] <= elem:
+            rr += 1
+            break
+        elif m[i][j] > elem:
+            rr += 1
 
+    for elem in get_col(m,j)[i-1::-1]:
+        if m[i][j] <= elem:
+            cu += 1
+            break
+        elif m[i][j] > elem:
+            cu += 1
+
+    for elem in get_col(m,j)[i+1:]:
+
+        if m[i][j] <= elem:
+            cd += 1
+            break
+        elif m[i][j] > elem:
+            cd += 1
+
+
+    # print(rl,rr,cu,cd)
+    return rl*rr*cu*cd
+    
+
+def part2():
+    n = len(data)
+    m = len(data[1])
+    massimo = 0
+    for i in range(1, n-1):
+        for j in range(1, m-1):
+            massimo = max(get_view_score(data,i,j),massimo)
+    return massimo
+
+
+
+print(part1())
+print(part2())
+# print(get_view_score(data,3,2))
 # print(data[3][1] > max(data[3][0:1]))
 # print(data[3][1] > max(data[3][2:]))
 # print(data[3][1] > max(get_col(data,1)[0:]))
